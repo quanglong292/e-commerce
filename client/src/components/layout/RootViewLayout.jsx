1a09e58acc8726ba8d6428bb1256b3167bc22198
quanglong292 <quang.long292@gmail.com>
2023-04-08 16:32:48 +0700

    [CLIENT]: redirect

--- client/src/components/layout/RootViewLayout.jsx
+++ client/src/components/layout/RootViewLayout.jsx
@@ -1,14 +1,25 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate, useResolvedPath } from "react-router-dom";
 import Navigation from "../core/Navigation";
-import { Suspense } from "react";
+import { Suspense, useEffect } from "react";
 import ComponentLoading from "./ComponentLoading";
 // import Sidebar from "../core/Sidebar";
 
 const RootViewLayout = () => {
+  const navigate = useNavigate();
+  const { pathname } = useResolvedPath();
+
+  useEffect(() => {
+    if (["/app"].includes(pathname)) navigate("/app/men");
+    if (["/"].includes(pathname)) navigate("/product");
+  }, [pathname]);
+
   return (
     <div className="w-full">
       <Navigation />
-      <div id="sidebar" className="flex items-start w-full max-w-[1440px] m-auto">
+      <div
+        id="sidebar"
+        className="flex items-start w-full max-w-[1440px] m-auto"
+      >
         <div
           id="detail"
           className="overflow-y-auto overflow-x-hidden w-full p-2"
