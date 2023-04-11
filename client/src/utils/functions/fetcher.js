379b435b7f02aa91918ee13643a9ddda67bc1a7f
quanglong292 <quang.long292@gmail.com>
2023-04-11 22:19:38 +0700

    [CLIENT]: prod base_url fix*3

--- client/src/utils/functions/fetcher.js
+++ client/src/utils/functions/fetcher.js
@@ -4,6 +4,13 @@ const BASE_URL_PROD = import.meta.env.VITE_BASE_URL_2;
 export default async function (requestParams, body) {
   const [urlPath, method] = requestParams;
   let BASE_URL = import.meta.env.PROD ? BASE_URL_PROD : BASE_URL_DEV;
+  console.log("fetcher", {
+    BASE_URL,
+    BASE_URL_DEV,
+    BASE_URL_PROD,
+    isProd: import.meta.env.PROD,
+    urlPath,
+  });
   const data = await fetch(BASE_URL + urlPath, {
     method,
     headers: {
