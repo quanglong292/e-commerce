2731c41bb84b5a3f3abaeeb2d5ef5e10c643db4a
Long Le <long.le@gbst.com>
2023-06-15 07:46:35 +0700

    auth footer, sort filter

--- server/src/controllers/product.controll.ts
+++ server/src/controllers/product.controll.ts
@@ -13,23 +13,22 @@ export const findProductGroup = async (category) => {
 export const handleGetProduct = async (query: IFilterOptions | any) => {
     if (query.name) query.name = { $regex: new RegExp(query.name, "i") }
     const sortQuery = cloneDeep(query)?.sortBy
-    delete sortQuery?.sortBy
+    delete query?.sortBy
     const isGetSaleProducts = query?.group === "761fcea4-58b4-4ce9-a4a5-fd5239228047"
     const saleData = await SaleModel.find()
     const productData = await ProductModel.find(isGetSaleProducts ? { id: uniq(saleData.map(i => i.products).flat()) } : query)
-
-    if (sortQuery === "highToLow") productData.sort({ price: 1 })
-    if (sortQuery === "lowToHigh") productData.sort({ price: -1 })
-
-    return productData.map(i => {
+    let combineSaleData = productData.map(i => {
         const item: any = cloneDeep(i.toJSON())
         const saleInfo: any = saleData.find(j => j.products.includes(item.id)) ?? { value: 0 }
-        console.log({ saleInfo });
 
-
+        // Mutate data
         item.saleInfo = { ...saleInfo?._doc }
         item.finalPrice = item.price - (item.price * Number(saleInfo.value) / 100)
 
         return item
     })
+
+    if (sortQuery) combineSaleData = combineSaleData.sort((a, b) => sortQuery === "highToLow" ? a.finalPrice - b.finalPrice : b.finalPrice - a.finalPrice)
+
+    return combineSaleData
 }
